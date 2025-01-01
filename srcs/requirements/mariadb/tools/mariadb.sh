5badc239ea4c74d84b32a21272d17393a3ee18dc
Sammy Gluck <infogls@yahoo.com>
2025-01-01 15:45:30 +0100

    take out .sql file setup because of credential exposing

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,45 +1,45 @@
 #!/bin/sh
 
-# Start MariaDB in the background
+# Start MariaDB in the background (with no networking so we can init)
 mysqld_safe --skip-networking &
 sleep 5
 
-# Modify bind-address to allow external connections
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+# Enable external connections inside Docker (i.e., 0.0.0.0)
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# Restart MariaDB to apply bind-address change
+# Restart MariaDB to apply config changes
 killall mysqld
 mysqld_safe --skip-networking &
 sleep 5
 
+if [ ! -d "/var/lib/mysql/${MARIADB_DATABASE}" ]; then
+    echo "Initializing new MariaDB database: $MARIADB_DATABASE"
 
-if [ ! -d "/var/lib/mysql/$MARIADB_DATABASE" ]; then
-# Secure MariaDB by executing SQL commands
-mysql -uroot <<-EOSQL
-    ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASS';
-    DELETE FROM mysql.user WHERE User='';
-    DROP DATABASE IF EXISTS test;
-    FLUSH PRIVILEGES;
-EOSQL
+    # Secure the root user and clean defaults
+    mysql -uroot <<-EOSQL
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASS}';
+        DELETE FROM mysql.user WHERE User='';
+        DROP DATABASE IF EXISTS test;
+        FLUSH PRIVILEGES;
+    EOSQL
+    
+    # Create your WP user / DB
+    mysql -uroot -p"${MARIADB_ROOT_PASS}" <<-EOSQL
+        CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
+        CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASS}';
+        GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
+        FLUSH PRIVILEGES;
+    EOSQL
 
-# Create database and user
-mysql -uroot -p"$MARIADB_ROOT_PASS" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS \`$MARIADB_DATABASE\`;
-    CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASS';
-    GRANT ALL PRIVILEGES ON \`$MARIADB_DATABASE\`.* TO '$MARIADB_USER'@'%';
-    FLUSH PRIVILEGES;
-EOSQL
-
-# Import the WordPress SQL dump
-mysql -uroot -p"$MARIADB_ROOT_PASS" "$MARIADB_DATABASE" < /usr/local/bin/wordpress.sql
-
+    echo "DB initialization complete."
 else
-	echo "Database already initialized. Skipping SQL dump import."
+    echo "Database already exists; skipping init."
 fi
 
-# Stop the background MariaDB process
-mysqladmin -uroot -p"$MARIADB_ROOT_PASS" shutdown
+# Stop background MySQL
+mysqladmin -uroot -p"${MARIADB_ROOT_PASS}" shutdown
 
-# Start MariaDB in the foreground
+# Finally run MySQL in the foreground
 exec "$@"
+
 
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,24 +1,41 @@
 #!/bin/sh
 
-#check if wp-config.php exist
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
+if [ -f wp-config.php ]; then
+    echo "WordPress already installed."
 else
-	#Download wordpress and all config file
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
+    echo "Downloading WordPress..."
+    wget http://wordpress.org/latest.tar.gz
+    tar -xzf latest.tar.gz
+    mv wordpress/* .
+    rm -rf latest.tar.gz wordpress
 
-	#Inport env variables in the config file
-	sed -i "s/username_here/$MARIADB_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MARIADB_PASS/g" wp-config-sample.php
-	sed -i "s/localhost/$MARIADB_HOST/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MARIADB_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+    # Insert DB credentials in wp-config
+    sed -i "s/database_name_here/${MARIADB_DATABASE}/" wp-config-sample.php
+    sed -i "s/username_here/${MARIADB_USER}/"         wp-config-sample.php
+    sed -i "s/password_here/${MARIADB_PASS}/"         wp-config-sample.php
+    sed -i "s/localhost/${MARIADB_HOST}/"             wp-config-sample.php
 
+    cp wp-config-sample.php wp-config.php
+
+    # Wait until DB is up (simple check - might suffice for 42)
+    echo "Waiting for MariaDB to be ready..."
+    until mysqladmin ping -h"${MARIADB_HOST}" -u"${MARIADB_USER}" -p"${MARIADB_PASS}" --silent; do
+      sleep 2
+    done
+    echo "MariaDB is up; proceeding."
+
+    # Run WP-CLI to finalize the WP installation
+    wp core install \
+      --url="${WP_URL}" \
+      --title="${WP_TITLE}" \
+      --admin_user="${WP_ADMIN_USER}" \
+      --admin_password="${WP_ADMIN_PASS}" \
+      --admin_email="${WP_ADMIN_EMAIL}" \
+      --skip-email
+
+    # If you want a non-admin user too:
+    wp user create "${WP_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASS}"
 fi
 
+# Hand over to php-fpm (CMD)
 exec "$@"
